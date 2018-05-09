02fb26897b3f5ab673164e063b01dff88b1af144
Emanuele Tonello <emanueletonello@gmail.com>
2018-05-09 16:30:59 +1000

    remove unused stuff

--- app/src/main/java/com/etapps/trovenla/Trove.java
+++ app/src/main/java/com/etapps/trovenla/Trove.java
@@ -3,13 +3,9 @@ package com.etapps.trovenla;
 import android.app.Application;
 
 import com.crashlytics.android.Crashlytics;
-import com.etapps.trovenla.utils.ConfigUtils;
 import com.etapps.trovenla.utils.TroveJobCreator;
 import com.evernote.android.job.JobManager;
-import com.facebook.stetho.Stetho;
 import com.google.firebase.analytics.FirebaseAnalytics;
-import com.twitter.sdk.android.core.TwitterAuthConfig;
-import com.uphyca.stetho_realm.RealmInspectorModulesProvider;
 
 import io.fabric.sdk.android.Fabric;
 import io.realm.Realm;
@@ -42,12 +38,6 @@ public class Trove extends Application {
 
         if (BuildConfig.DEBUG) {
             Timber.plant(new Timber.DebugTree());
-
-            Stetho.initialize(
-                    Stetho.newInitializerBuilder(this)
-                            .enableDumpapp(Stetho.defaultDumperPluginsProvider(this))
-                            .enableWebKitInspector(RealmInspectorModulesProvider.builder(this).build())
-                            .build());
         }
 
         JobManager.create(this).addJobCreator(new TroveJobCreator());
